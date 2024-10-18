0e2bf87dd41ab5e12331e4d341085659dbd21906
Ana Júlia Mantovani <138818583+Anajuliamantovani@users.noreply.github.com>
2024-10-18 17:32:32 -0300

    Case 6 funcional

--- Projeto Nava/main.c
+++ Projeto Nava/main.c
@@ -23,12 +23,12 @@ int main()
     {
         case 1:
             Id = Add(Id);
-        break;
+            break;
 
         case 2:
             printf("Qual usuario deseja deletar? \n");
-        Delete(Id);
-        break;
+            Delete(Id);
+            break;
 
         case 3:
             edit(Id);
@@ -36,13 +36,16 @@ int main()
 
         case 4:
             printf("Qual usuario deseja visualizar ? \n");
-        print(Id);
-        break;
+            print(Id);
+            break;
         case 5:
             PrintAll(Id);
-        break;
+            break;
 
         case 6:
+            Order(Id);
+            printf("Cadastro ordenado por odem alfabetica\n");
+            DadosOrdenados(Id);
             break;
 
         case 7:
